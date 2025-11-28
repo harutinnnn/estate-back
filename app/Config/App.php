9a7672da9253024aef5f2fb1ce08d9913e2ac121
harut <harutinnnn@gmail.com>
2025-11-28 17:06:17 +0400

    First commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://ci4.local/';
+    public string $baseURL = 'http://estate-back.local/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
