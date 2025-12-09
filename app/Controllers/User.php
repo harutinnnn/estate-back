222551d5d34bb1771b172d8fb317f7937541c1b0
harut <harutinnnn@gmail.com>
2025-12-09 11:31:55 +0400

    First commit

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -31,6 +31,7 @@ class User extends MainController
         }
 
 
+
         $categoriesModel = new CategoryModel();
         $categories = $categoriesModel->getAllItems($this->_lang, 0, [], ['col' => 'pos', 'sort' => 'ASC']);
         $this->pageData['categories'] = array_column($categories, 'title', 'id');
